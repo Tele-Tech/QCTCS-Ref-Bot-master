6a2670b5859cd6930293eb84a3b758e3f26ab0cb
Tele-Tech <69707557+Tele-Tech@users.noreply.github.com>
2020-10-06 20:19:44 +0530

    Initial commit

---
+++ data.js
@@ -0,0 +1,18 @@
+module.exports = {
+    token: '1268921453:AAEdFR6yykIZqBQ0bqVcPQ8biskM87pKPLc', // token Bot
+    mongoLink: '', // e.g. Mongo Atlas
+    channels: [''], // channel(s) ID after -100 
+    inviteLink: 't.me/Technical_Guybot?start=params', // params - only user id or something extra
+    admins: ['@Technical_Guy'], // admin(s) ID
+    channel_link_1: '@Tech_Giveaways',
+    channel_link_2: '@Tech_Proofs',
+    channel_link_3: '@Freefireindia_a',
+    networkURL: '',
+    feedbackBot: '',
+    feedbackURL: '',
+    nChan: 3,
+    eur4usr10: 0.1, // cent of €/invite
+    minInv: 100, // number of minimum invites
+    checkoutDay: 1 //Lunedì
+    //timer: 172800000 //48h in ms
+}
